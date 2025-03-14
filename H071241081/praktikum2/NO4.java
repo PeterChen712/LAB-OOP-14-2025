2c14944dfa2d4d6ca0a7cc041f3d3a06d9079236
myrella10 <ahmadh6755@gmail.com>
2025-03-14 15:30:53 +0800

    TP2 kumpul

---
+++ H071241081/praktikum2/NO4.java
@@ -0,0 +1,44 @@
+package TP2;
+
+class Alamat {
+    String jalan ;
+    String kota ;
+}
+
+class Mahasiswa {
+    String nama ;
+    String nim ;
+    Alamat alamat ;
+
+    public String getNama() {
+        return nama;
+    }
+    public String getNim() {
+        return nim;
+    }
+   
+    public String getAlamat () {
+        return alamat.jalan + ", " + alamat.kota;
+    }
+
+    
+}
+
+public class NO4 {
+    public static void main(String[] args) {
+        Alamat alamat = new Alamat();
+        alamat.jalan = "Tamalancea Indah";
+        alamat.kota = "Makassar";
+
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.alamat = alamat;
+        mahasiswa.nama = "Farhan";
+        mahasiswa.nim = "H071231025";
+
+        System.out.println("Nama: " + mahasiswa.getNama());
+        System.out.println("Nim: " + mahasiswa.getNim());
+        System.out.println("Alamat: " + mahasiswa.getAlamat());
+    }
+}
+    
+
